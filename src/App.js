19f2611e5ac0b6cbcb68eddfd7caf7dbf084c723
Derryk Boyd <derrykboyd@gmail.com>
2019-10-27 00:38:52 -0600

    Add remote delete and update functionality.
    Fixed the DB sync and state management issues. Remote DB loads to state on first load.
    Local item added gets added to state and synced to remote DB.
    Remote deletes, additions and edits get synced and added to state.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,79 @@
 import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import db, { addItem } from './db';
+import * as db from './db';
 import TodoItem from './Components/TodoItem';
 
 function App() {
 
-  
-
   const [items, setItems] = useState([]);
   const [newItem, setNewItem] = useState("")
+
+  useEffect(() => {
+    // set initial items from the remote DB - only runs once
+    function getData() {
+      db.remoteDB.allDocs({ include_docs: true }).then(res => {
+        console.log(res);
+        let fetchedItems = [];
+        res.rows.map(row => fetchedItems.push(row.doc));
+        setItems([...fetchedItems]);
+      });
+    }
+    getData();
+  }, [])
+
+  // This effect handles changes to the db from other clients
+  useEffect(() => {
+
+    const handleRemoteDelete = (id) => {
+      let newItems = [...items];
+      let delIndex = newItems.findIndex(el => el._id === id);
+      if (delIndex === -1) return;
+      newItems.splice(delIndex, 1);
+      setItems(newItems);
+      console.log(`Deleted: ${id}`);
+    }
+
+    const handleRemoteUpdate = (doc) => {
+      let newItems = [...items];
+      let updateIndex = newItems.findIndex(el => el._id === doc._id);
+      if (updateIndex !== -1) {
+        newItems[updateIndex] = doc;
+        setItems(newItems);
+        return;
+      } else {
+        newItems.push(doc);
+        setItems(newItems);
+      }
+    }
+    
+    let dbSync = db.localDB.sync(db.remoteDB, {
+      live: true,
+      retry: true,
+      include_docs: true,
+    }).on('change', (e) => {
+      console.log('Database Changed');
+      console.log(e);
+      let itemChanged = e.change.docs[0];
+      if (itemChanged._deleted && e.direction === 'pull') {
+        console.log(`Item deleted: ${itemChanged._id}`)
+        handleRemoteDelete(itemChanged._id);
+      } else if (e.direction === 'pull') {
+        // Handle update or insert
+        handleRemoteUpdate(itemChanged);
+        console.log(`Updataed or Inserted: ${itemChanged._id}`)
+      } else {
+        console.log('This was a local change');
+      }
+    }).on('active', () => {
+      console.log('sync active')
+    }).on('error', () => {
+      console.log('Database Error')
+    });
+    return () => {
+      dbSync.cancel();
+    };
+  }, [items])
 
   const handleSubmit = (e) => {
     let itemToAdd = {
@@ -17,26 +81,11 @@ function App() {
       todo: newItem,
       completed: false,
     }
-    addItem(itemToAdd)
+    db.addItem(itemToAdd)
     setItems([...items, itemToAdd]);
     setNewItem("");
     e.preventDefault();
   }
-
-  useEffect(() => {
-    let blankTodo = {
-      _id: new Date().toISOString(),
-      todo: "",
-      completed:false,
-    }
-    function getData() {
-      db.allDocs({ include_docs: true }).then(res => {
-        let items = res.rows.map(row => row.doc);
-        setItems([blankTodo, ...items])
-      });
-    }
-    getData();
-  }, [])
 
   return (
     <div className="App">
--- src/db.js
+++ src/db.js
@@ -1,28 +1,22 @@
 import PouchDB from 'pouchdb';
 
-let db = new PouchDB('todo-app');
+let localDB = new PouchDB('todo-app');
 let host = window.location.hostname;
 console.log(host);
 let remoteDB = new PouchDB(`http://${host}:5984/todo-app`);
 
-db.sync(remoteDB, {
-    live: true,
-    include_docs: true,
-}).on('change', (e) => {
-    console.log('Database Changed')
-    console.log(e)
-    if (e.deleted) {
-        console.log(`Item deleted: ${e.change.docs}`)
-    } else {
-        console.log(`Updataed or Inserted: ${e.change.docs}`)
-    }
-}).on('error', () => {
-    console.log('Database Error')
-});
-
 export async function addItem(item) {
     console.log(item);
-    db.put(item)
+    localDB.put(item)
 }
 
-export default db;
+export async function deleteItem(item) {
+    item._deleted = true;
+    localDB.put(item);
+}
+
+export async function updateItem(item) {
+
+}
+
+export {localDB, remoteDB};
