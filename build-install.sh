d655de02c19f3d8085bb3accd6c50eef960b6b68
Derryk Boyd <derrykboyd@gmail.com>
2019-11-05 22:46:05 -0700

    Add build process

--- build-install.sh
+++ build-install.sh
@@ -1,6 +1,14 @@
-#!/bin/bash -x
+#!/bin/bash
 
 PATH="/root/.nvm/versions/node/v13.0.1/bin:$PATH"
 
-pwd
-npm -v
+set +x
+
+# update npm packages
+npm install
+
+# build production site
+npm run build
+
+# copy files to server location
+rsync -rvh ./build/ /var/www/db-todo/
