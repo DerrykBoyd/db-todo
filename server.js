0557c21726f79130299bce46f5df4921447c839f
Derryk Boyd <derrykboyd@gmail.com>
2019-12-30 02:11:12 -0700

    Update server logic to create db async

--- server.js
+++ server.js
@@ -2,29 +2,27 @@ const express = require('express');
 const helmet = require('helmet');
 const cors = require('cors');
 require('dotenv').config();
-const http = require('http')
 
 // Create Express app
 const app = express();
 app.use(helmet());
 app.use(cors());
 
-let testObj = { test: 'test' };
 const DB_USER = process.env.DB_USER;
 const DB_PASS = process.env.DB_PASS;
 const DB_HOST = process.env.DB_HOST;
 const DB_PORT = process.env.DB_PORT;
+const SERV_PORT = 4000;
 
 // Convert DB credentials to base64
 let dbAuth = `${DB_USER}:${DB_PASS}`;
-let buff = new Buffer.from(dbAuth);
-let dbAuth64 = buff.toString('base64');
 
-console.log(dbAuth64);
+// connect nano to couchDB
+const nano = require('nano')(`http://${dbAuth}@${DB_HOST}:${DB_PORT}`);
 
 // test if server is running
 app.get('/test', (req, res) => {
-    res.send('Server Running.')
+    res.send(`Server Running on port ${SERV_PORT}`)
 })
 
 // Route for creating a new DB if none existing
@@ -32,30 +30,14 @@ app.put('/:dbName', (req, res) => {
 
     let dbName = req.params.dbName;
 
-    let options = {
-        hostname: DB_HOST,
-        port: DB_PORT,
-        path: `/${dbName}`,
-        method: 'PUT',
-        headers: {
-            Accept: 'application/json',
-            Authorization: `Basic ${dbAuth64}`,
-            Host: DB_HOST,
-        },
-    }
-
-    const request = http.request(options, res => {
-        console.log(`statusCode: ${res.statusCode}`)
-
-        res.on('data', d => {
-            process.stdout.write(d)
-        })
+    // use nano to create new DB and send res back to client
+    nano.db.create(dbName).then(body => {
+        res.send('Database Created')
+    }).catch(err => {
+        if (err.error === 'file_exists') res.send('DB already exists')
     })
-    request.on('error', err => console.log(err));
-    request.end();
 
-    res.send(JSON.stringify(testObj));
 })
 
 // Start the Express server
-app.listen(4000, () => console.log('Server running on port 4000!'))
+app.listen(SERV_PORT, () => console.log('Server running on port 4000'))
