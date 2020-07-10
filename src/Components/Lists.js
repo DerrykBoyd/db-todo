691666b6ef8deb39564d9c6cdfdf7ee3f00a5029
Derryk Boyd <derrykboyd@gmail.com>
2020-07-10 01:02:19 -0600

    Fix list not loading on google login

--- src/Components/Lists.js
+++ src/Components/Lists.js
@@ -7,49 +7,53 @@ import ListMenu from './ListMenu';
 // component to display user lists
 export default function Lists(props) {
 
-    useEffect(() => {
-        document.title = 'Todo-My Lists'
-    }, []);
+  useEffect(() => {
+    document.title = 'Todo-My Lists'
+  }, []);
 
-    const toggleLists = () => {
-        let cur = props.showLists;
-        props.setShowLists(!cur);
-    }
+  const toggleLists = () => {
+    let cur = props.showLists;
+    props.setShowLists(!cur);
+  }
 
-    return (
-        <div className={`App Lists`}>
-            <Header
-                handleLogout={props.handleLogout}
-                loggedIn={props.dbUser}
-                userImg={props.dbUser.profileURL}
-                currentListName={props.currentListName}
-                handleListChange={props.handleListChange} />
-            <TodoList
-                items={props.items}
-                setItems={props.setItems}
-                updateItems={props.updateItems}
-                handleItemUpdate={props.handleItemUpdate}
-                handleLocalAdd={props.handleLocalAdd}
-                handleItemChange={props.handleItemChange}
-                handleChecked={props.handleChecked}
-                deleteItem={props.deleteItem}>
-            </TodoList>
-            <TodoAdd
-                newItem={props.newItem}
-                handleNewChange={props.handleNewChange}
-                handleLocalAdd={props.handleLocalAdd}
-                showLists={props.showLists}
-                toggleLists={toggleLists}>
-            </TodoAdd>
-            {props.showLists &&
-                <ListMenu 
-                    lists={props.lists}
-                    updateLists={props.updateLists}
-                    deleteList={props.deleteList}
-                    switchList={props.switchList}
-                    setShowLists={props.setShowLists}
-                />
-            }
-        </div>
-    )
+  return (
+    <div className={`App Lists`}>
+      {props.dbUser &&
+        <>
+          <Header
+            handleLogout={props.handleLogout}
+            loggedIn={props.dbUser}
+            userImg={props.dbUser.profileURL}
+            currentListName={props.currentListName}
+            handleListChange={props.handleListChange} />
+          <TodoList
+            items={props.items}
+            setItems={props.setItems}
+            updateItems={props.updateItems}
+            handleItemUpdate={props.handleItemUpdate}
+            handleLocalAdd={props.handleLocalAdd}
+            handleItemChange={props.handleItemChange}
+            handleChecked={props.handleChecked}
+            deleteItem={props.deleteItem}>
+          </TodoList>
+          <TodoAdd
+            newItem={props.newItem}
+            handleNewChange={props.handleNewChange}
+            handleLocalAdd={props.handleLocalAdd}
+            showLists={props.showLists}
+            toggleLists={toggleLists}>
+          </TodoAdd>
+          {props.showLists &&
+            <ListMenu
+              lists={props.lists}
+              updateLists={props.updateLists}
+              deleteList={props.deleteList}
+              switchList={props.switchList}
+              setShowLists={props.setShowLists}
+            />
+          }
+        </>
+      }
+    </div>
+  )
 }
