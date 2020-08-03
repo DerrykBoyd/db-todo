1657595d4025b435a9a52b90ec485adae4553dac
Derryk Boyd <derrykboyd@gmail.com>
2020-08-03 17:23:41 -0600

    Add screenshot to home page

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -5,20 +5,31 @@ import Credits from './Credits';
 
 // Firebase
 import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth';
+import '../styles/Home.css';
+import screenshot from '../assets/db-todo-screenshot_iphonexspacegrey_portrait.webp';
 
 export default function Home(props) {
 
-    useEffect(() => {
-      document.title = 'Todo'
-    }, []);
-  
-    return (
-      <div className={`App`}>
+  useEffect(() => {
+    document.title = 'Todo'
+  }, []);
+
+  return (
+    <div className={`App`}>
+      <div className="home-content">
         <StyledFirebaseAuth
           uiConfig={props.uiConfig}
           firebaseAuth={props.firebaseApp.auth()}
         />
+        <div className="screenshot">
+          <img className="screenshot"
+            src={screenshot}
+            alt="app-screenshot" />
+        </div>
+      </div>
+      <div className='footer'>
         <Credits />
       </div>
-    )
-  }
+    </div>
+  )
+}
